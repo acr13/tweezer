3dc2a0f794d1ef8f22e4c18c06ed69b3dd00a60c
Alex Richardson <alex@alexcr.com>
2021-05-02 17:57:25 -0400

    Add got - add start command, games router

--- app/index.js
+++ app/index.js
@@ -2,10 +2,12 @@ import Koa from 'koa';
 import Router from 'koa-router';
 
 import { healthRouter } from './routes/health/index.js';
+import { gamesRouter } from './routes/games/games.router.js';
 
 const app = new Koa();
 const router = new Router()
-  .use('/health', healthRouter.routes());
+  .use('/health', healthRouter.routes())
+  .use('/games', gamesRouter.routes());
 
 app
   .use(router.routes())
